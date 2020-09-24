cf55574f9256995180db3dd4a06b13874ba0a9f7
Aiden Marques <aiden.marques94@gmail.com>
2020-09-24 13:39:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog 
   
   def name=(dogs_name)
+<<<<<<< HEAD
     @name = dogs_name
   end
   
@@ -8,6 +9,11 @@ class Dog
     @name 
   end
   
+=======
+    @this_dogs_name = dogs_name
+  end
+   
+>>>>>>> 8a1476c59d7b2ebeed9d3b38530432d2e8b5264c
   def bark 
     puts "woof!"
   end 
@@ -15,5 +21,9 @@ class Dog
 end
 
 fido = Dog.new 
+<<<<<<< HEAD
 fido.name= "Fido"
+=======
+fido.name="Fido"
+>>>>>>> 8a1476c59d7b2ebeed9d3b38530432d2e8b5264c
 
